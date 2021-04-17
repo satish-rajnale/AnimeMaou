13c6f393efa145ab758db39eee7123955ae97a5a
satish-rajnale <satishrajnale98@gmail.com>
2021-04-17 22:55:36 +0530

    main.scss

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,29 +2,34 @@ function Sidebar({ topAnime }) {
   return (
     <aside>
       <nav>
-        <h3>Top Anime</h3>
+    
+      <h3>Top Anime</h3>
+       
         {topAnime.map((anime) => (
-          <div  className="top-list">
-            <img key={anime.mal_id} src={anime.image_url} />
+          <div key={anime.mal_id} className="top-list">
+            <img  src={anime.image_url} alt={anime.title} />
             <div className="top-text">
             <a
               href={anime.url}
               target="_blank"
-              key={anime.mal_id}
+             
               rel="noreferrer"
             >
               {anime.title}
             </a>
-            <p key={anime.mal_id}>rating: {anime.score}</p>
+            <p >rating: {anime.score}</p>
             </div>
           </div>
         ))}
       </nav>
       <nav>
-        {" "}
+      <div  className="top-list">
+      <div className="top-text">
         <a href="#" target="_blank" key="more" rel="noreferrer">
           More...
         </a>
+        </div>
+          </div>
       </nav>
     </aside>
   );
